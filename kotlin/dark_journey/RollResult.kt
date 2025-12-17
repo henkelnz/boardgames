a217cc129ac5da624cff9f2acc75722d8fc7e194
Nathan Henkel <nathanzhenkel@gmail.com>
2025-12-16 22:18:32 -0500

    restructuring

--- kotlin/dark_journey/RollResult.kt
+++
@@ -1,9 +0,0 @@
-/**
- * Represents the combined result of one or more die rolls.
- */
-data class RollResult(
-    val miss: Boolean = false,
-    val range: Int = 0,
-    val surges: Int = 0,
-    val wounds: Int = 0
-)
